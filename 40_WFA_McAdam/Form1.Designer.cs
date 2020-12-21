cb50224ab35a2e480f070869e93a0a3d6faf371c
Selimcan <sccdemir@gmail.com>
2020-12-21 11:26:04 +0300

    first commit

---
+++ 40_WFA_McAdam/Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿
+namespace _40_WFA_McAdam
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbMenu = new System.Windows.Forms.ComboBox();
+            this.rbOrta = new System.Windows.Forms.RadioButton();
+            this.rbBuyuk = new System.Windows.Forms.RadioButton();
+            this.rbKing = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.chkRanch = new System.Windows.Forms.CheckBox();
+            this.chkCheddar = new System.Windows.Forms.CheckBox();
+            this.chkBuffalo = new System.Windows.Forms.CheckBox();
+            this.chkHardal = new System.Windows.Forms.CheckBox();
+            this.chkAciSos = new System.Windows.Forms.CheckBox();
+            this.chkBarbeku = new System.Windows.Forms.CheckBox();
+            this.lblAdet = new System.Windows.Forms.Label();
+            this.nudAdet = new System.Windows.Forms.NumericUpDown();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnSiparisVer = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudAdet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(185, 140);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 159);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Menü";
+            // 
+            // cmbMenu
+            // 
+            this.cmbMenu.FormattingEnabled = true;
+            this.cmbMenu.Location = new System.Drawing.Point(54, 159);
+            this.cmbMenu.Name = "cmbMenu";
+            this.cmbMenu.Size = new System.Drawing.Size(143, 21);
+            this.cmbMenu.TabIndex = 2;
+            // 
+            // rbOrta
+            // 
+            this.rbOrta.AutoSize = true;
+            this.rbOrta.Location = new System.Drawing.Point(12, 191);
+            this.rbOrta.Name = "rbOrta";
+            this.rbOrta.Size = new System.Drawing.Size(45, 17);
+            this.rbOrta.TabIndex = 3;
+            this.rbOrta.TabStop = true;
+            this.rbOrta.Text = "Orta";
+            this.rbOrta.UseVisualStyleBackColor = true;
+            // 
+            // rbBuyuk
+            // 
+            this.rbBuyuk.AutoSize = true;
+            this.rbBuyuk.Location = new System.Drawing.Point(78, 191);
+            this.rbBuyuk.Name = "rbBuyuk";
+            this.rbBuyuk.Size = new System.Drawing.Size(55, 17);
+            this.rbBuyuk.TabIndex = 3;
+            this.rbBuyuk.TabStop = true;
+            this.rbBuyuk.Text = "Büyük";
+            this.rbBuyuk.UseVisualStyleBackColor = true;
+            // 
+            // rbKing
+            // 
+            this.rbKing.AutoSize = true;
+            this.rbKing.Location = new System.Drawing.Point(152, 191);
+            this.rbKing.Name = "rbKing";
+            this.rbKing.Size = new System.Drawing.Size(46, 17);
+            this.rbKing.TabIndex = 3;
+            this.rbKing.TabStop = true;
+            this.rbKing.Text = "King";
+            this.rbKing.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.chkBarbeku);
+            this.groupBox1.Controls.Add(this.chkBuffalo);
+            this.groupBox1.Controls.Add(this.chkAciSos);
+            this.groupBox1.Controls.Add(this.chkCheddar);
+            this.groupBox1.Controls.Add(this.chkHardal);
+            this.groupBox1.Controls.Add(this.chkRanch);
+            this.groupBox1.Location = new System.Drawing.Point(16, 231);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(181, 100);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ekstralar";
+            // 
+            // chkRanch
+            // 
+            this.chkRanch.AutoSize = true;
+            this.chkRanch.Location = new System.Drawing.Point(7, 20);
+            this.chkRanch.Name = "chkRanch";
+            this.chkRanch.Size = new System.Drawing.Size(58, 17);
+            this.chkRanch.TabIndex = 0;
+            this.chkRanch.Text = "Ranch";
+            this.chkRanch.UseVisualStyleBackColor = true;
+            // 
+            // chkCheddar
+            // 
+            this.chkCheddar.AutoSize = true;
+            this.chkCheddar.Location = new System.Drawing.Point(6, 43);
+            this.chkCheddar.Name = "chkCheddar";
+            this.chkCheddar.Size = new System.Drawing.Size(66, 17);
+            this.chkCheddar.TabIndex = 0;
+            this.chkCheddar.Text = "Cheddar";
+            this.chkCheddar.UseVisualStyleBackColor = true;
+            // 
+            // chkBuffalo
+            // 
+            this.chkBuffalo.AutoSize = true;
+            this.chkBuffalo.Location = new System.Drawing.Point(6, 66);
+            this.chkBuffalo.Name = "chkBuffalo";
+            this.chkBuffalo.Size = new System.Drawing.Size(59, 17);
+            this.chkBuffalo.TabIndex = 0;
+            this.chkBuffalo.Text = "Buffalo";
+            this.chkBuffalo.UseVisualStyleBackColor = true;
+            // 
+            // chkHardal
+            // 
+            this.chkHardal.AutoSize = true;
+            this.chkHardal.Location = new System.Drawing.Point(101, 20);
+            this.chkHardal.Name = "chkHardal";
+            this.chkHardal.Size = new System.Drawing.Size(57, 17);
+            this.chkHardal.TabIndex = 0;
+            this.chkHardal.Text = "Hardal";
+            this.chkHardal.UseVisualStyleBackColor = true;
+            // 
+            // chkAciSos
+            // 
+            this.chkAciSos.AutoSize = true;
+            this.chkAciSos.Location = new System.Drawing.Point(100, 43);
+            this.chkAciSos.Name = "chkAciSos";
+            this.chkAciSos.Size = new System.Drawing.Size(62, 17);
+            this.chkAciSos.TabIndex = 0;
+            this.chkAciSos.Text = "Acı Sos";
+            this.chkAciSos.UseVisualStyleBackColor = true;
+            // 
+            // chkBarbeku
+            // 
+            this.chkBarbeku.AutoSize = true;
+            this.chkBarbeku.Location = new System.Drawing.Point(100, 66);
+            this.chkBarbeku.Name = "chkBarbeku";
+            this.chkBarbeku.Size = new System.Drawing.Size(66, 17);
+            this.chkBarbeku.TabIndex = 0;
+            this.chkBarbeku.Text = "Barbekü";
+            this.chkBarbeku.UseVisualStyleBackColor = true;
+            // 
+            // lblAdet
+            // 
+            this.lblAdet.AutoSize = true;
+            this.lblAdet.Location = new System.Drawing.Point(16, 338);
+            this.lblAdet.Name = "lblAdet";
+            this.lblAdet.Size = new System.Drawing.Size(32, 13);
+            this.lblAdet.TabIndex = 5;
+            this.lblAdet.Text = "Adet:";
+            // 
+            // nudAdet
+            // 
+            this.nudAdet.Location = new System.Drawing.Point(78, 338);
+            this.nudAdet.Name = "nudAdet";
+            this.nudAdet.Size = new System.Drawing.Size(120, 20);
+            this.nudAdet.TabIndex = 6;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(219, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(488, 316);
+            this.listBox1.TabIndex = 7;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnEkle.FlatAppearance.BorderColor = System.Drawing.Color.Blue;
+            this.btnEkle.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnEkle.ForeColor = System.Drawing.Color.Snow;
+            this.btnEkle.Location = new System.Drawing.Point(219, 334);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(99, 31);
+            this.btnEkle.TabIndex = 8;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = false;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnSiparisVer
+            // 
+            this.btnSiparisVer.BackColor = System.Drawing.Color.Tan;
+            this.btnSiparisVer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSiparisVer.ForeColor = System.Drawing.Color.AliceBlue;
+            this.btnSiparisVer.Location = new System.Drawing.Point(608, 334);
+            this.btnSiparisVer.Name = "btnSiparisVer";
+            this.btnSiparisVer.Size = new System.Drawing.Size(99, 31);
+            this.btnSiparisVer.TabIndex = 8;
+            this.btnSiparisVer.Text = "Sipariş Ver";
+            this.btnSiparisVer.UseVisualStyleBackColor = false;
+            this.btnSiparisVer.Click += new System.EventHandler(this.btnSiparisVer_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(719, 377);
+            this.Controls.Add(this.btnSiparisVer);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.nudAdet);
+            this.Controls.Add(this.lblAdet);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.rbKing);
+            this.Controls.Add(this.rbBuyuk);
+            this.Controls.Add(this.rbOrta);
+            this.Controls.Add(this.cmbMenu);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudAdet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbMenu;
+        private System.Windows.Forms.RadioButton rbOrta;
+        private System.Windows.Forms.RadioButton rbBuyuk;
+        private System.Windows.Forms.RadioButton rbKing;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox chkBarbeku;
+        private System.Windows.Forms.CheckBox chkBuffalo;
+        private System.Windows.Forms.CheckBox chkAciSos;
+        private System.Windows.Forms.CheckBox chkCheddar;
+        private System.Windows.Forms.CheckBox chkHardal;
+        private System.Windows.Forms.CheckBox chkRanch;
+        private System.Windows.Forms.Label lblAdet;
+        private System.Windows.Forms.NumericUpDown nudAdet;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnSiparisVer;
+    }
+}
+
---
+++ 40_WFA_McAdam/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _40_WFA_McAdam
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //ADDS ITEMS TO MENU ON LOAD
+
+            cmbMenu.Items.Add("McChicken");
+            cmbMenu.Items.Add("Whooper");
+            cmbMenu.Items.Add("Big King");
+            cmbMenu.Items.Add("Texas Smoke House");
+        }
+        int price = 0;
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            //MAKING A STRING FORMAT OF ALL THE OPTIONS SO WE CAN ADD THEM LATER
+
+            //CHECKOUT PRICE
+
+            
+            if (cmbMenu.SelectedIndex == 0)
+            {
+                price = 15;
+            }
+            if (cmbMenu.SelectedIndex == 1)
+            {
+                price = 20;
+            }
+            if (cmbMenu.SelectedIndex == 2)
+            {
+                price =22;
+            }
+            if (cmbMenu.SelectedIndex == 3)
+            {
+                price = 30;
+            }
+
+
+            //MENU
+
+            string menu = "";
+            
+
+            //SIZE
+            string size = "";
+            if (rbOrta.Checked == true)
+            {
+                size = "Orta boyut";
+            }
+            if (rbBuyuk.Checked == true)
+            {
+                size = "Büyük boyut";
+                price += 2;
+            }
+            if (rbKing.Checked == true)
+            {
+                size = "King boyut";
+                price += 5;
+            }
+            //SAUCES
+            string sauce = "";
+            if (chkRanch.Checked)
+            {
+                sauce = "-Ranch sos-";
+                price += 1;
+            }
+            if (chkHardal.Checked)
+            {
+                sauce += "-Hardal sos-";
+                price += 1;
+            }
+            if (chkCheddar.Checked)
+            {
+                sauce += "-Cheddar sos-";
+                price += 1;
+            }
+            if (chkAciSos.Checked)
+            {
+                sauce += "-Acı sos-";
+                price += 1;
+            }
+            if (chkBuffalo.Checked)
+            {
+                sauce += "-Bufalo sos-";
+                price += 1;
+            }
+            if (chkBarbeku.Checked)
+            {
+                sauce += "-Barbekü sos-";
+                price += 1;
+            }
+            //AMOUNT
+            decimal adet = 0;
+            adet = nudAdet.Value;
+            price *= Convert.ToInt32(adet);
+
+
+            //ADD
+            if (cmbMenu.SelectedIndex > -1)
+            {
+                cmbMenu.SelectedItem.ToString();
+                listBox1.Items.Add(string.Format("{0} ,{1} ,{2}, {3} tane, {4} TL", menu, size, sauce, adet, price));
+
+
+            }
+            if (cmbMenu.SelectedIndex == -1)
+            {
+                MessageBox.Show("Bir menü seçiniz");
+
+            }
+            
+        }
+
+        private void btnSiparisVer_Click(object sender, EventArgs e)
+        {
+            decimal adet = nudAdet.Value;                              
+            MessageBox.Show(adet.ToString() + " adet menü ve " + price.ToString() + " TL", "Siparişinizde bulunan adet ve ödemeniz gereken toplam:");
+            DialogResult result = MessageBox.Show("Ödemek istiyormusunuz?" , "Devam", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+            if (result == DialogResult.Yes)
+            {
+                foreach (Control ctrl in this.Controls)
+                {
+                    if (ctrl is ComboBox)
+                    {
+                        ComboBox cb = (ComboBox)ctrl;
+                        cb.SelectedIndex = -1;
+                    }
+                    if (ctrl is GroupBox)
+                    {
+                        GroupBox gbx = (GroupBox)ctrl;
+                        foreach (Control gctrl in gbx.Controls)
+                        {
+                            if(gctrl is CheckBox)
+                            {
+                                CheckBox cb = (CheckBox)gctrl;
+                                cb.Checked = false;
+                            }
+                        }
+                        
+                    }
+                    if (ctrl is RadioButton)
+                    {
+                        RadioButton rb = (RadioButton)ctrl;
+                        rb.Checked = false;
+                    }
+                    if (ctrl is NumericUpDown)
+                    {
+                        NumericUpDown nud = (NumericUpDown)ctrl;
+                        nud.Value = 0;
+                    }
+                    if (ctrl is ListBox)
+                    {
+                        ListBox lb = (ListBox)ctrl;
+                        lb.Items.Clear();
+                    }
+                }
+            }
+            else
+            {
+                
+            }
+        }
+    }
+}
+
+
+
+
